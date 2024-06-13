b6d2880795ffde89d3a7692bf4df5602ab7e6455
khaidprogramming <khalidmiya399@gmail.com>
2024-06-13 07:19:52 +0545

    iniital setup the project

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  return (
+    <div>
+      {isAuthenticated ? (<Navigate to="/" />) : (<>
+      <section>
+        <Outlet/>
+        </section>
+        </>)}
+    </div>
+  )
+}
+
+export default AuthLayout
